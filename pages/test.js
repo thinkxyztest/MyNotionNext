2820a69780ca029e9f58d25fc6517ffc23df05c4
thinkxyztest <thinkxyztest@gmail.com>
2025-01-08 21:39:47 +0800

    Add files via upload

---
+++ pages/test.js
@@ -0,0 +1,10 @@
+// pages/test.js
+export default function TestPage() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>测试页面</h1>
+      <p>这是一个简短的测试页面。</p>
+      <p>如果你能看到这段内容，说明 Next.js 配置正常！</p>
+    </div>
+  );
+}
